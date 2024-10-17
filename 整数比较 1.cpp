d0d2e69e89bda13a5cd51f4d9a69dfb6843ed57e
Edward Blaster <98396298+WHTblaster@users.noreply.github.com>
2024-10-17 10:57:09 +0800

    Add files via upload

---
+++ 整数比较 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a > b) {
+		printf("%d %d", a % 10, b * b);
+	}
+	else {
+		printf("%d %d", b % 10, a * a);
+	}
+
+	return 0;
+}
